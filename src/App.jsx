3cd3af97d34ee28fc7a25bd19bed69027f96bcd9
Oraya Chantanawat <ochantanawat@gmail.com>
2025-03-20 12:03:22 +0700

    repo(setup): create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import AboutMe from "./components/AboutMe";
+
 export default function App() {
   return (
-    <div className="flex p-6">
-      <h1 className="text-amber-500">React App Project Starter</h1>
+    <div className="h-screen flex flex-col justify-start p-6 bg-amber-50">
+      <h1 className="text-stone-700 font-bold">React App Project Starter</h1>
+      <section className="w-full p-5 text-stone-600 bg-red-100 flex">
+        <ul className="list-inside list-disc flex-1">
+          <span className="font-semibold">Tech Stack:</span>
+          <li>Vite</li>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>Tailwind</li>
+        </ul>
+      </section>
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
     </div>
-  )
+  );
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <div className='bg-amber-200 text-red-400 pl-6 pr-6'>About Me</div>
+  )
+}
+
+export default AboutMe
