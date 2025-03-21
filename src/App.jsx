4c3593378e851a9451aee8e8f01b1f42cf41e671
Oraya Chantanawat <ochantanawat@gmail.com>
2025-03-21 12:26:57 +0700

    repo(setup): edit starter template

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import AboutMe from "./components/AboutMe";
 
 export default function App() {
   return (
-    <div className="h-screen flex flex-col justify-start p-6 bg-amber-50">
-      <h1 className="text-stone-700 font-bold">React App Project Starter</h1>
-      <section className="w-full p-5 text-stone-600 bg-red-100 flex">
-        <ul className="list-inside list-disc flex-1">
-          <span className="font-semibold">Tech Stack:</span>
-          <li>Vite</li>
-          <li>React</li>
-          <li>JavaScript</li>
-          <li>Tailwind</li>
-        </ul>
-      </section>
-      <AboutMe />
+    <div className="h-screen flex justify-center bg-stone-950 text-white">
+      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+        <h1 className="w-full p-6 bg-zinc-700 font-bold rounded-xl">
+          React App Starter
+        </h1>
+        <section className="w-full p-5 bg-zinc-500 flex rounded-xl">
+          <ul className="list-inside list-disc flex-1">
+            <span className="font-semibold">Tech Stack:</span>
+            <li>Vite</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </section>
+      <AboutMe name="Aliz 🐝" />
+      </div>
     </div>
   );
 }
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function AboutMe() {
+function AboutMe(props) {
   return (
-    <div className='bg-amber-200 text-red-400 pl-6 pr-6'>About Me:</div>
+    <div className='bg-amber-200 text-teal-800 rounded-2xl pl-6 pr-6'><span className='font-bold'>About me</span>: {props.name}</div>
   )
 }
 
